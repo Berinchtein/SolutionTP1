9ac6ca3ba678a0a26f5fd6e7d4cd7e998ced1586
Manuel Jarry <lemanujarry@gmail.com>
2025-01-22 21:05:11 -0500

    Ajout de calculs dans probleme 1

--- Probleme2/main.cpp
+++ Probleme2/main.cpp
@@ -6,19 +6,21 @@ using namespace std;
 int main() {
 
 	// Déclaration des variables
-	float somme_argent;
+	float somme_emprunt;
 	float montant_mensuel;
 	float taux_annuel;
 	float taux_mensuel;
+	float somme_interet = 0;
+	unsigned nb_mois = 0;
 
 	// Entrée de la somme d'argent et vérification de sa validité
 	do {
 		cout << "\nEntrez la somme d'argent qui a ete pretee: ";
-		cin >> somme_argent;
-		if (somme_argent < 0) {
+		cin >> somme_emprunt;
+		if (somme_emprunt < 0) {
 			cout << "La somme d'argent entree n'est pas positive. Veuillez recommencer.";
 		}
-	} while (somme_argent < 0);
+	} while (somme_emprunt < 0);
 
 	// Entrée du montant remboursé chaque mois et vérification de sa validité
 	do {
@@ -39,6 +41,23 @@ int main() {
 	} while (taux_annuel < 0 || taux_annuel > 100);
 
 	taux_mensuel = taux_annuel / 12;
+
+	while (somme_emprunt > 0) {
+		if (somme_emprunt - montant_mensuel >= 0) {
+			somme_emprunt -= montant_mensuel;
+			somme_emprunt *= (1 + (taux_mensuel / 100.0));
+			somme_interet += somme_emprunt * (taux_mensuel / 100.0);
+		}
+		else {
+			somme_emprunt = 0;
+		}
+		//cout << somme_emprunt << endl;
+		nb_mois += 1;
+	}
+
+	cout << "Nombre de mois: " << nb_mois << endl;
+
+	cout << "Somme percue par le preteur: " << somme_interet << endl;
 
 	return 0;
 }
