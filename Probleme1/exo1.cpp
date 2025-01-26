61826d304de166efda940b59a08c3a54cff81a4e
Manuel Jarry <manuel.jarry@polymtl.ca>
2025-01-26 18:22:36 -0500

    Variable(s) de exo1 renommée(s) pour respecter la convention

--- Probleme1/exo1.cpp
+++ Probleme1/exo1.cpp
@@ -1,3 +1,8 @@
+/*
+ * Nom: Probleme 1 TP2
+ * Description: Fichier principal du probleme #1 pour le TP2
+ * Auteurs: Massylia Aggoun et Manuel Jarry
+ */
 
 #include <iostream>
 
@@ -5,23 +10,23 @@ using namespace std;
 
 int main() {
 
-	int input_number;
+	int inputNumber;
 
 	cout << "Entrez un nombre entier: ";
-	cin >> input_number;
+	cin >> inputNumber;
 
-	if (input_number == -1 || input_number == 0 || input_number == 1) {
+	if (inputNumber == -1 || inputNumber == 0 || inputNumber == 1) {
 		cout << "Ce nombre n'est pas premier car -1, 0 et 1 ne sont pas des nombres premiers.";
 		return 0;
 	}
 
-	if (input_number != 2 && input_number % 2 == 0) {
+	if (inputNumber != 2 && inputNumber % 2 == 0) {
 		cout << "Ce nombre n'est pas premier car il est divisible par 2.";
 		return 0;
 	}
 
-	for (int i = 3; i <= ceil(sqrt(input_number)); i += 2) {
-		if (input_number % i == 0) {
+	for (int i = 3; i <= ceil(sqrt(inputNumber)); i += 2) {
+		if (inputNumber % i == 0) {
 			cout << "Ce nombre n'est pas premier car il est divisible par " << i << ".";
 			return 0;
 		}
